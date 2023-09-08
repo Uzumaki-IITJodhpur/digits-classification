2c43a6c22de7c46d6e1663e6d8511c41404a4394
Uzumaki-IITJodhpur <m22aie245@iitj.ac.in>
2023-09-08 20:24:54 +0530

    hyper_param and test_dev combination with github actions

--- experiments_modular.py
+++ experiments_modular.py
@@ -1,4 +1,4 @@
-from digit_classification import DigitClassification
+from utils import DigitClassification
 
 digit_classification = DigitClassification()
 digit_classification.load_dataset()
@@ -6,14 +6,14 @@ x, y = digit_classification.load_digit_data()
 digit_classification.plot_data(x, y, execution="training data")
 digit_classification.initiate_svm(gamma=0.001)
 x = digit_classification.pre_process(x)
-x_train, x_test, y_train, y_test  = digit_classification.split_data(x, y, test_split_ratio=0.3, random_state=42)
-digit_classification.train_model(x_train, y_train)
-y_test_predicted = digit_classification.predict(x_test)
+X_train, X_test, y_train, y_test  = digit_classification.split_data(x, y, test_split_ratio=0.3, random_state=42)
+digit_classification.train_model(X_train, y_train)
+y_test_predicted = digit_classification.predict(X_test)
 digit_classification.print_classification_report(y_test_predicted, y_test, "test-set")
 digit_classification.display_confusion_matrix(y_test_predicted, y_test, "test-set")
 
-x_train, x_dev, x_test, y_train, y_val, y_test  = digit_classification.split_data_train_dev_test(x, y, dev_size=0.15, test_size=0.15, random_state=42)
-y_dev_predicted = digit_classification.predict(x_dev)
+X_train, X_dev, X_test, y_train, y_val, y_test  = digit_classification.split_data_train_dev_test(x, y, dev_size=0.15, test_size=0.15, random_state=42)
+y_dev_predicted = digit_classification.predict(X_dev)
 digit_classification.print_classification_report(y_dev_predicted, y_test, "dev-set")
 digit_classification.display_confusion_matrix(y_dev_predicted, y_test, "dev-set")
 print("Complete")
