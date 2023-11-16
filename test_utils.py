9668a159ae96dbfcfb004e2c49f3cc4bdb11e799
uzumaki-IITJodhpur <m22aie245@iitj.ac.in>
2023-11-17 03:08:29 +0530

    added test case to check data splits

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,6 @@
+from utils import DigitClassification
 from itertools import product
+import numpy as np
 
 def test_hparam_combination_count():
     gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
@@ -16,3 +18,15 @@ def test_hparam_combination_values():
 
     assert ( expected_param_combo_1 in list_of_all_param_combination ) \
         and ( expected_param_combo_2 in list_of_all_param_combination )
+
+def test_data_split():
+    digit_classification = DigitClassification()
+    digit_classification.load_dataset()
+    x, y = digit_classification.load_digit_data()
+    test_split, dev_split = 0.1, 0.2
+    train_split = 1 - test_split - dev_split
+    X_train, X_dev, X_test, y_train, y_dev, y_test  = digit_classification.split_data_train_dev_test(x, y, dev_size=dev_split, test_size=test_split, random_state=42)
+    
+    assert (len(X_train) == int(train_split*len(x))) \
+        and (len(X_dev) == int(np.ceil(dev_split*len(x)))) \
+        and (len(X_test) == int(np.ceil(test_split*len(x))))
